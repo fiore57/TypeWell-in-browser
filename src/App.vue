c616761841c651614eb9cecb0f1eb04514e8d4c6
fiore57 <fiore57@pc-gear.com>
2020-01-25 00:44:40 +0900

    Don't scroll when space key pressed

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,37 @@
   <div id="app">
     <!--<img alt="Vue logo" src="./assets/logo.png">-->
     <TypeWell msg="test"/>
+    <Help/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import TypeWell from './components/TypeWell.vue';
+import Help from './components/Help.vue';
 
 @Component({
   components: {
     TypeWell,
+    Help,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  public keyInput(event: KeyboardEvent) {
+    // スペースでスクロールするのを止める
+    if(event.key === " "){
+      event.preventDefault(); // デフォルトアクションを止める
+    }
+  }
+
+  beforeMount() {
+    window.addEventListener('keydown', this.keyInput, true);
+  }
+
+  beforeDestroy() {
+    window.removeEventListener('keydown', this.keyInput, true);
+  }
+}
 </script>
 
 <style lang="scss">
