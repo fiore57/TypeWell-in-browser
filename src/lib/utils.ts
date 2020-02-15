07027d8a604e427ed7496b00b3fe7abe1f79ee99
fiore57 <fiore57@pc-gear.com>
2020-02-15 22:17:05 +0900

    Add clamp() and step()

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,14 +1,13 @@
-// get randum integer [min, max)
+/** get randum integer [min, max) */
 export function getRandomInt(min: number, max: number): number;
-// get random integer [0, max)
+/** get random integer [0, max) */
 export function getRandomInt(max: number): number;
 export function getRandomInt(val1: number, val2?: number): number {
   if (val2 !== undefined) {
     const min = Math.ceil(val1);
     const max = Math.floor(val2);
     return Math.floor(Math.random() * (max - min)) + min;
-  }
-  else {
+  } else {
     return getRandomInt(0, val1);
   }
 }
@@ -20,6 +19,27 @@ declare global {
   }
 }
 /** 配列の最初の要素を返す（右辺値） */
-(Array.prototype as any).front = function () { return this[0]; };
+(Array.prototype as any).front = function() {
+  return this[0];
+};
 /** 配列の最後の要素を返す（右辺値） */
-(Array.prototype as any).back = function () { return this[this.length - 1]; };
+(Array.prototype as any).back = function() {
+  return this[this.length - 1];
+};
+
+/** val を [low, high] に収める */
+export function clamp(val: number, low: number, high: number): number {
+  return Math.min(Math.max(val, low), high);
+}
+
+/**
+ * val を超えない最小の step の倍数を返す
+ *
+ * 引数は整数にすること！！！
+ *
+ * @param val 整数
+ * @param step 整数
+ */
+export function step(val: number, step: number): number {
+  return Math.floor(val / step) * step;
+}
--- tests/unit/utils.spec.ts
+++ tests/unit/utils.spec.ts
@@ -1,10 +1,9 @@
-
-import { getRandomInt } from '@/lib/utils'
+import { getRandomInt, clamp, step } from "@/lib/utils";
 
 const loop = 100;
 
-describe('getRandomInt(k)', () => {
-  it('結果は0以上k未満の整数', () => {
+describe("getRandomInt(k)", () => {
+  it("結果は0以上k未満の整数", () => {
     const k = 3;
     for (let i = 0; i < loop; ++i) {
       const rand = getRandomInt(k);
@@ -12,11 +11,11 @@ describe('getRandomInt(k)', () => {
       expect(rand).toBeLessThan(k);
       expect(rand % 1).toEqual(0);
     }
-  })
+  });
 });
 
-describe('getRandomInt(a, b)', () => {
-  it('結果はa以上b未満の整数', () => {
+describe("getRandomInt(a, b)", () => {
+  it("結果はa以上b未満の整数", () => {
     const a = 10298;
     const b = 34857;
     for (let i = 0; i < loop; ++i) {
@@ -25,15 +24,40 @@ describe('getRandomInt(a, b)', () => {
       expect(rand).toBeLessThan(b);
       expect(rand % 1).toEqual(0);
     }
-  })
-})
+  });
+});
 
-describe('front() and back()', () => {
+describe("front() and back()", () => {
   const arr: number[] = [12, 48, 83, 18, 7, 3, 981, 25];
-  it('front()は先頭の要素を指す', () => {
+  it("front()は先頭の要素を指す", () => {
     expect(arr.front()).toEqual(12);
-  })
-  it('back()は最後の要素を指す', () => {
+  });
+  it("back()は最後の要素を指す", () => {
     expect(arr.back()).toEqual(25);
-  })
-})
+  });
+});
+
+describe("clamp(val, low, high)", () => {
+  const low = 35;
+  const high = 93.2;
+  it("val < low ならば戻り値は low", () => {
+    expect(clamp(34.999999, low, high)).toEqual(low);
+  });
+  it("low <= val <= high ならば戻り値は val", () => {
+    const val1 = 35.000001;
+    const val2 = 93.199999;
+    expect(clamp(val1, low, high)).toEqual(val1);
+    expect(clamp(val2, low, high)).toEqual(val2);
+  });
+  it("high < val ならば戻り値は high", () => {
+    expect(clamp(93.200001, low, high)).toEqual(high);
+  });
+});
+
+describe("step(val, step)", () => {
+  it("戻り値は val を超えない最小の step の倍数", () => {
+    expect(step(512, 10)).toEqual(510);
+    expect(step(42.8, 2.5)).toEqual(42.5);
+    expect(step(48, 2)).toEqual(48);
+  });
+});
