b232372d3f05ab399ce9b92b30e08b489993cd0c
fiore57 <fiore57@pc-gear.com>
2020-02-28 01:55:20 +0900

    Refactor roman's scss

--- src/components/TypeWellRoman.vue
+++ src/components/TypeWellRoman.vue
@@ -53,22 +53,24 @@ export default createComponent({
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+@import "@/assets/variable.scss";
 .roman {
-  margin: 1rem auto;
+  @include white-block;
+  margin: 1rem;
   width: 58rem;
-  height: 21.5rem;
-  border: 0.1rem solid;
-  border-color: gray;
+  height: $roman-block-height;
   font-size: 1.8rem;
   font-family: "Noto Sans Mono", sans-serif;
-  background: white;
+  display: flex; // 子要素をflexboxで揃える
+  flex-direction: column; // 縦方向
+  justify-content: space-between; // 縦方向に広げて配置
 }
 .roman-line {
-  margin: 0.2rem 1rem;
-  display: flex; /* 子要素をflexboxで揃える */
-  flex-direction: row; /* 子要素をflexboxにより横方向に揃える */
-  justify-content: flex-start; /* 子要素をflexboxにより左に配置する */
-  align-items: center; /* 子要素をflexboxにより中央に配置する */
+  margin: 0 0 0.1rem 0;
+  display: flex; // 子要素をflexboxで揃える
+  flex-direction: row; // 横方向
+  justify-content: flex-start; // 水平方向左
+  align-items: center; // 垂直方向中央
   white-space: pre;
   word-break: break-all;
 }
